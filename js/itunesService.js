d485df5395145e38e85baafb6b36adb9edf3be5c
Ray Pena <rhayme00@gmail.com>
2017-06-08 20:56:13 -0600

    This project is done

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,14 +1,33 @@
-angular.module('itunes').service('itunesService', function($http, $q){
-  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+angular.module('itunes').service('itunesService', function($http, $q) {
+    //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
+    //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
-  //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
-  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+    //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+    //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+    //Note that in the above line, artist is the parameter being passed in. 
+    //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+    this.getSongData = function(artist) {
+        return $http({
+            method: 'JSONP',
+            url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+        }).then(function(response) {
+            console.log(response);
+            var users = response.data.results;
+            users.forEach(function(song) {
+                song.Play = song.previewUrl;
+                song.Artist = song.artistName;
+                song.Song = song.trackName;
+                song.Collection = song.collectionName;
+                song.AlbumArt = song.artworkUrl30;
+                song.Type = song.primaryGenreName;
+                song.CollectionPrice = song.collectionPrice;
+            });
+            return users;
+        })
+
+    }
 
 
 
@@ -25,8 +44,8 @@ angular.module('itunes').service('itunesService', function($http, $q){
       Play: "http://a423.phobos.apple.com/us/r1000/013/Music4/v4/4a/ab/7c/4aab7ce2-9a72-aa07-ac6b-2011b86b0042/mzaf_6553745548541009508.plus.aac.p.m4a"
       Type: "song"
   */
-  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this finalized array of data. 
-  // When this is complete, head back to your controller.
+    //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this finalized array of data. 
+    // When this is complete, head back to your controller.
 
 
 });
